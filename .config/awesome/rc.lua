ac584b445d7b405710880574f946b38f123a725a
Oscar Linde <ocalinde@axis.com>
2015-08-17 10:57:02 +0200

    Removed some unused layouts from awesome config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -61,8 +61,6 @@ layouts =
     awful.layout.suit.tile,
     awful.layout.suit.floating,
     awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
     awful.layout.suit.max
 }
 -- }}}
