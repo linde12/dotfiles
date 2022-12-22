bd54bf74665b3c3b0e5fb2ca59ca12078ae60461
linde12 <lindeoscar@gmail.com>
2022-12-22 18:30:51 +0100

    pipewire/xdp sleeps... the software is still kind of buggy

--- river.sh
+++ river.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export MOZ_ENABLE_WAYLAND=1
 export XDG_SESSION_TYPE=wayland
-export XDG_CURRENT_DESKTOP=XFCE
+export XDG_CURRENT_DESKTOP=river
 
 timestamp=$(date +%F-%R)
 exec dbus-run-session river > /tmp/river-${timestamp}.log 2>&1
